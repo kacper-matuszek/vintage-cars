b392b528c4d7e5f3875f28ce37291373e076cc8b
Kacper <kacpermatuszek2@gmail.com>
2020-08-08 20:26:30 +0200

    Query base

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Commands/Base/QueryBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace VintageCars.Domain.Commands.Base
+{
+    public abstract class QueryBase<TResult> : IRequest<TResult>
+        where TResult: class
+    {
+    }
+}
