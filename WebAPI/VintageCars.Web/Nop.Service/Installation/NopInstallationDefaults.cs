8751ab5c3457634d28372390603d68a7024367ac
Kacper <kacpermatuszek2@gmail.com>
2020-08-18 21:46:31 +0200

    Create NopInstallationDefaults.cs

---
+++ WebAPI/VintageCars.Web/Nop.Service/Installation/NopInstallationDefaults.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Service.Installation
+{
+    public static partial class NopInstallationDefaults
+    {
+        /// <summary>
+        /// Gets a path to the localization resources file
+        /// </summary>
+        public static string LocalizationResourcesPath => "~/App_Data/Localization/";
+
+        /// <summary>
+        /// Gets a localization resources file extension
+        /// </summary>
+        public static string LocalizationResourcesFileExtension => "nopres.xml";
+    }
+}
