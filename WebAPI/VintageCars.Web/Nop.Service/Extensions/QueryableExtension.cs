4c97057b0fcd00e8bce617303d7d956f8a511b01
Kacper <kacpermatuszek2@gmail.com>
2020-08-18 21:45:26 +0200

    Create QueryableExtension.cs

---
+++ WebAPI/VintageCars.Web/Nop.Service/Extensions/QueryableExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Nop.Core.Caching;
+using Nop.Core.Infrastructure;
+
+namespace Nop.Service.Extensions
+{
+    public static class QueryableExtension
+    {
+        private static IStaticCacheManager CacheManager => EngineContext.Current.Resolve<IStaticCacheManager>();
+
+        /// <summary>
+        /// Gets a cached first element of a sequence, or a default value
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="query">Elements of source to put on cache</param>
+        /// <param name="cacheKey">Cache key</param>
+        /// <returns>Cached first element or default value</returns>
+        public static T ToCachedFirstOrDefault<T>(this IQueryable<T> query, CacheKey cacheKey)
+        {
+            return cacheKey == null
+                ? query.FirstOrDefault()
+                : CacheManager.Get(cacheKey, query.FirstOrDefault);
+        }
+    }
+}
