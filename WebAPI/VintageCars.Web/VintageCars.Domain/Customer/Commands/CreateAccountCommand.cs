9b3b20fb8ffad2162826bb29eeefc7f017b3495b
Kacper <kacpermatuszek2@gmail.com>
2020-08-19 20:44:55 +0200

    Create CreateAccountCommand.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Customer/Commands/CreateAccountCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using VintageCars.Domain.Settings.Base;
+
+namespace VintageCars.Domain.Customer.Commands
+{
+    public class CreateAccountCommand : GoogleRecaptchaBase, IRequest
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
