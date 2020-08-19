5fc8fa9ba396562bb04bd9ef1b4e0e94ec44dc07
Kacper <kacpermatuszek2@gmail.com>
2020-08-19 20:41:11 +0200

    Create CustomerRegistrationRequest.cs

---
+++ WebAPI/VintageCars.Web/Framework/Nop.Core/Requests/Customers/CustomerRegistrationRequest.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Core.Domain.Customers;
+
+namespace Nop.Core.Requests.Customers
+{
+    public class CustomerRegistrationRequest
+    {
+        public Customer Customer { get; set; }
+        public string Password { get; set; }
+        public PasswordFormat PasswordFormat { get; set; }
+        public int StoreId { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
