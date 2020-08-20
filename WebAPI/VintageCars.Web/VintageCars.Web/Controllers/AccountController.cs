68193c19ac9bef0b263adbc7d4259a25cd46e7b4
Kacper <kacpermatuszek2@gmail.com>
2020-08-20 20:28:49 +0200

    Create AccountController.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Web/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VintageCars.Domain.Customer.Commands;
+
+namespace VintageCars.Web.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AccountController : BaseController
+    {
+        public AccountController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register([FromBody] CreateAccountCommand account)
+            => await ExecuteCommandWithoutResult(account);
+    }
+}
