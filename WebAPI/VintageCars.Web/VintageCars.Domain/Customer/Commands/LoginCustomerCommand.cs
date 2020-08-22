3af9fb99bd51c97346dcced159a9e427eceb7f4b
Kacper <kacpermatuszek2@gmail.com>
2020-08-22 17:12:55 +0200

    Create LoginCustomerCommand.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Customer/Commands/LoginCustomerCommand.cs
@@ -0,0 +1,12 @@
+﻿using VintageCars.Domain.Commands.Base;
+using VintageCars.Domain.Customer.Responses;
+
+namespace VintageCars.Domain.Customer.Commands
+{
+    public class LoginCustomerCommand : CommandBase<LoginCustomerResponse>
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
