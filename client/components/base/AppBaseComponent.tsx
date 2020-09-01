03ac0edfc07cc6220568d7adb65b9b0a50553891
Kacper <kacpermatuszek2@gmail.com>
2020-09-01 22:01:03 +0200

    Added extended head

--- client/components/base/AppBaseComponent.tsx
+++ client/components/base/AppBaseComponent.tsx
@@ -1,20 +1,21 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
+import React, { ReactNode, ReactElement } from 'react'
 import Head from 'next/head'
 import { ThemeProvider, Container, Grid } from '@material-ui/core'
 import { theme, useStyles } from '../theme'
 
 type Props = {
     children?: ReactNode
-    title?: string
+    title?: string,
+    head?: ReactElement
 }
-
-const AppBase = ({children, title}: Props) => (
+const AppBase = ({children, title, head}: Props) => {
+    return(
     <div>
         <Head>
             <title>{title}</title>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+            {head}
         </Head>
         <ThemeProvider theme={theme}>
             <Grid container className={useStyles().root}>
@@ -22,6 +23,5 @@ const AppBase = ({children, title}: Props) => (
             </Grid>
         </ThemeProvider>
     </div>
-)
-
-export default AppBase
+)}
+export default AppBase;
