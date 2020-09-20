dfac827127997735dc2563c0198150e4b3d76df2
Kacper <kacpermatuszek2@gmail.com>
2020-09-20 09:00:23 +0200

    Created queued email service

--- WebAPI/VintageCars.Web/VintageCars.Web/Configuration/Registration/DependencyRegister.cs
+++ WebAPI/VintageCars.Web/VintageCars.Web/Configuration/Registration/DependencyRegister.cs
@@ -70,7 +70,8 @@ namespace VintageCars.Web.Configuration.Registration
             builder.RegisterType<MessageTokenProvider>().As<IMessageTokenProvider>().InstancePerLifetimeScope();
             builder.RegisterType<MessageTemplateService>().As<IMessageTemplateService>().InstancePerLifetimeScope();
             builder.RegisterType<EmailAccountService>().As<IEmailAccountService>().InstancePerLifetimeScope();
-            builder.RegisterType<Tokenizer>().As<ITokenizer>();
+            builder.RegisterType<Tokenizer>().As<ITokenizer>().InstancePerLifetimeScope();
+            builder.RegisterType<QueuedEmailService>().As<IQueuedEmailService>().InstancePerLifetimeScope();
 
             builder.RegisterType<ActionContextAccessor>().As<IActionContextAccessor>().InstancePerLifetimeScope();
 
