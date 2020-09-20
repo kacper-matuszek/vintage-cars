df2a0b93cba8ef7e13e82a0458d4cb44066ac63f
Kacper <kacpermatuszek2@gmail.com>
2020-09-20 09:27:28 +0200

    Create WorkflowMessageService.cs

---
+++ WebAPI/VintageCars.Web/Nop.Service/Messages/WorkflowMessageService.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nop.Core;
+using Nop.Core.Domain.Messages;
+using Nop.Service.Customer;
+using Nop.Service.Localization;
+
+namespace Nop.Service.Messages
+{
+    public partial class WorkflowMessageService : IWorkflowMessageService
+    {
+        private readonly IStoreContext _storeContext;
+        private readonly ILanguageService _languageService;
+        private readonly IMessageTemplateService _messageTemplateService;
+        private readonly IMessageTokenProvider _messageTokenProvider;
+        private readonly ICustomerService _customerService;
+        private readonly IEmailAccountService _emailAccountService;
+        private readonly EmailAccountSettings _emailAccountSettings;
+        private readonly ITokenizer _tokenizer;
+        private readonly IQueuedEmailService _queuedEmailService;
+
+        public WorkflowMessageService(IStoreContext storeContext,
+            ILanguageService languageService,
+            IMessageTemplateService messageTemplateService,
+            IMessageTokenProvider messageTokenProvider,
+            ICustomerService customerService,
+            IEmailAccountService emailAccountService,
+            EmailAccountSettings emailAccountSettings,
+            ITokenizer tokenizer,
+            IQueuedEmailService queuedEmailService)
+        {
+            _storeContext = storeContext;
+            _languageService = languageService;
+            _messageTemplateService = messageTemplateService;
+            _messageTokenProvider = messageTokenProvider;
+            _customerService = customerService;
+            _emailAccountService = emailAccountService;
+            _emailAccountSettings = emailAccountSettings;
+            _tokenizer = tokenizer;
+            _queuedEmailService = queuedEmailService;
+        }
+
+        #region Utilities
+        /// <summary>
+        /// Get active message templates by the name
+        /// </summary>
+        /// <param name="messageTemplateName">Message template name</param>
+        /// <param name="storeId">Store identifier</param>
+        /// <returns>List of message templates</returns>
+        protected virtual IList<MessageTemplate> GetActiveMessageTemplates(string messageTemplateName, Guid storeId)
+        {
+            //get message templates by the name
+            var messageTemplates = _messageTemplateService.GetMessageTemplatesByName(messageTemplateName, storeId);
+
+            //no template found
+            if (!messageTemplates?.Any() ?? true)
+                return new List<MessageTemplate>();
+
+            //filter active templates
+            messageTemplates = messageTemplates.Where(messageTemplate => messageTemplate.IsActive).ToList();
+
+            return messageTemplates;
+        }
+
+        /// <summary>
+        /// Ensure language is active
+        /// </summary>
+        /// <param name="languageId">Language identifier</param>
+        /// <param name="storeId">Store identifier</param>
+        /// <returns>Return a value language identifier</returns>
+        protected virtual Guid EnsureLanguageIsActive(Guid languageId, Guid storeId)
+            => _languageService.GetLanguageById(languageId)?.Id ?? throw new Exception("No active language could be loaded");
+
+        /// <summary>
+        /// Get EmailAccount to use with a message templates
+        /// </summary>
+        /// <param name="messageTemplate">Message template</param>
+        /// <param name="languageId">Language identifier</param>
+        /// <returns>EmailAccount</returns>
+        protected virtual EmailAccount GetEmailAccountOfMessageTemplate(MessageTemplate messageTemplate, Guid languageId)
+        {
+            var emailAccount = (_emailAccountService.GetEmailAccountById(messageTemplate.EmailAccountId) ?? _emailAccountService.GetEmailAccountById(_emailAccountSettings.DefaultEmailAccountId)) ??
+                               _emailAccountService.GetAllEmailAccounts().FirstOrDefault();
+            return emailAccount;
+        }
+        #endregion
+
+        #region Customer workflow
+        /// <summary>
+        /// Sends password recovery message to a customer
+        /// </summary>
+        /// <param name="customer">Customer instance</param>
+        /// <param name="languageId">Message language identifier</param>
+        /// <returns>Queued email identifier</returns>
+        public virtual IList<Guid> SendCustomerPasswordRecoveryMessage(Core.Domain.Customers.Customer customer, Guid languageId)
+        {
+            if (customer == null)
+                throw new ArgumentNullException(nameof(customer));
+
+            var store = _storeContext.CurrentStore;
+            languageId = EnsureLanguageIsActive(languageId, store.Id);
+
+            var messageTemplates = GetActiveMessageTemplates(MessageTemplateSystemNames.CustomerPasswordRecoveryMessage, store.Id);
+            if (!messageTemplates.Any())
+                return new List<Guid>();
+
+            //tokens
+            var commonTokens = new List<Token>();
+            _messageTokenProvider.AddCustomerTokens(commonTokens, customer);
+
+            return messageTemplates.Select(messageTemplate =>
+            {
+                //email account
+                var emailAccount = GetEmailAccountOfMessageTemplate(messageTemplate, languageId);
+
+                var tokens = new List<Token>(commonTokens);
+                _messageTokenProvider.AddStoreTokens(tokens, store, emailAccount);
+
+                var toEmail = customer.Email;
+                var toName = _customerService.GetCustomerFullName(customer);
+
+                return SendNotification(messageTemplate, emailAccount, languageId, tokens, toEmail, toName);
+            }).ToList();
+        }
+
+        /// <summary>
+        /// Send notification
+        /// </summary>
+        /// <param name="messageTemplate">Message template</param>
+        /// <param name="emailAccount">Email account</param>
+        /// <param name="languageId">Language identifier</param>
+        /// <param name="tokens">Tokens</param>
+        /// <param name="toEmailAddress">Recipient email address</param>
+        /// <param name="toName">Recipient name</param>
+        /// <param name="attachmentFilePath">Attachment file path</param>
+        /// <param name="attachmentFileName">Attachment file name</param>
+        /// <param name="replyToEmailAddress">"Reply to" email</param>
+        /// <param name="replyToName">"Reply to" name</param>
+        /// <param name="fromEmail">Sender email. If specified, then it overrides passed "emailAccount" details</param>
+        /// <param name="fromName">Sender name. If specified, then it overrides passed "emailAccount" details</param>
+        /// <param name="subject">Subject. If specified, then it overrides subject of a message template</param>
+        /// <returns>Queued email identifier</returns>
+        public virtual Guid SendNotification(MessageTemplate messageTemplate,
+            EmailAccount emailAccount, Guid languageId, IEnumerable<Token> tokens,
+            string toEmailAddress, string toName,
+            string attachmentFilePath = null, string attachmentFileName = null,
+            string replyToEmailAddress = null, string replyToName = null,
+            string fromEmail = null, string fromName = null, string subject = null)
+        {
+            if (messageTemplate == null)
+                throw new ArgumentNullException(nameof(messageTemplate));
+
+            if (emailAccount == null)
+                throw new ArgumentNullException(nameof(emailAccount));
+
+            //retrieve localized message template data
+            var bcc = messageTemplate.BccEmailAddresses;
+            if (string.IsNullOrEmpty(subject))
+                subject = messageTemplate.Subject;
+            var body = messageTemplate.Body;
+
+            //Replace subject and body tokens 
+            var subjectReplaced = _tokenizer.Replace(subject, tokens, false);
+            var bodyReplaced = _tokenizer.Replace(body, tokens, true);
+
+            //limit name length
+            toName = CommonHelper.EnsureMaximumLength(toName, 300);
+
+            var email = new QueuedEmail
+            {
+                Priority = QueuedEmailPriority.High,
+                From = !string.IsNullOrEmpty(fromEmail) ? fromEmail : emailAccount.Email,
+                FromName = !string.IsNullOrEmpty(fromName) ? fromName : emailAccount.DisplayName,
+                To = toEmailAddress,
+                ToName = toName,
+                ReplyTo = replyToEmailAddress,
+                ReplyToName = replyToName,
+                CC = string.Empty,
+                Bcc = bcc,
+                Subject = subjectReplaced,
+                Body = bodyReplaced,
+                AttachmentFilePath = attachmentFilePath,
+                AttachmentFileName = attachmentFileName,
+                AttachedDownloadId = messageTemplate.AttachedDownloadId,
+                CreatedOnUtc = DateTime.UtcNow,
+                EmailAccountId = emailAccount.Id,
+                DontSendBeforeDateUtc = !messageTemplate.DelayBeforeSend.HasValue ? null
+                    : (DateTime?)(DateTime.UtcNow + TimeSpan.FromHours(messageTemplate.DelayPeriod.ToHours(messageTemplate.DelayBeforeSend.Value)))
+            };
+
+            _queuedEmailService.InsertQueuedEmail(email);
+            return email.Id;
+        }
+        #endregion
+    }
+}
