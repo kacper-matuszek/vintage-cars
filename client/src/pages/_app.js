33b195b10811efbfe87bd1e5913866a2fa3141cc
Kacper <kacpermatuszek2@gmail.com>
2020-10-01 00:05:23 +0200

    Show success alert

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -24,6 +24,8 @@ export default function App({Component, pageProps, router}) {
     const [showErrorRespText, setErrorRespText] = useState("");
     const [showValidationResponse, setValidationResponse] = useState(false);
     const [showValidationRespText, setValidationRespText] =  useState("");
+    const [showSuccess, setSuccess] = useState(false);
+    const [showSuccessText, setSuccessText] = useState("");
 
     const handleError = (event, reason) => {
         if (reason === 'clickaway') {
@@ -53,6 +55,20 @@ export default function App({Component, pageProps, router}) {
             setValidationResponse(true);
         }
     }
+    
+    const handleSuccess = (event, reason) => {
+        if(reson === 'clickway')
+            return;
+        setSuccess(false);
+        setSuccessText("");
+    }
+
+    const handleShowSuccess = (content) => {
+        if(!isEmpty(content)) {
+            setSuccessText(content);
+            setSuccess(true);
+        }
+    }
 
     const checkAuthorization = () => {
         const value = new Cookie().get(CookieDictionary.Token);
@@ -62,18 +78,21 @@ export default function App({Component, pageProps, router}) {
     return (
         <AppBase title={pageProps.title} loading={loading}
         showError={showErrorResponse} errorMessage={showErrorRespText} handleError={handleError}
-        showValidation={showValidationResponse} validationMessage={showValidationRespText}>
+        showValidation={showValidationResponse} validationMessage={showValidationRespText} handleSuccess={handleSuccess}
+        showSuccess={showSuccess} showSuccessMessage={showSuccessText}>
             {router.pathname.startsWith('/login') || router.pathname.startsWith('/register') ? 
             <PictureContent>
                 <Component {...pageProps} 
                 setLoading={setLoading}
                 showError={handleShowError}
-                showWarning={handleShowWarning}/>
+                showWarning={handleShowWarning}
+                showSuccess={handleSuccess}/>
             </PictureContent> : <MainLayout isAuthorized={checkAuthorization()}>
             <Component {...pageProps} 
             setLoading={setLoading}
             showError={handleShowError}
-            showWarning={handleShowWarning}/>
+            showWarning={handleShowWarning}
+            showSuccess={handleSuccess}/>
             </MainLayout>
             }
         <style jsx global>
