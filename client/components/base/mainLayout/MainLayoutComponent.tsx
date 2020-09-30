ba579d5a50eb06f7ce44c79eadb3590637740443
Kacper <kacpermatuszek2@gmail.com>
2020-09-30 22:31:59 +0200

    Add route menu items in main layout.

--- client/components/base/mainLayout/MainLayoutComponent.tsx
+++ client/components/base/mainLayout/MainLayoutComponent.tsx
@@ -4,7 +4,7 @@ import PersonIcon from '@material-ui/icons/Person'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import { NameWithNode, RouterWithElement } from '../../../core/models/base/NameWithNode'
 import DirectionsCarIcon from '@material-ui/icons/DirectionsCar';
-import { generateMenuItems, generateLinkMenuItems } from '../../shared/generatorUtils/GeneratorExtension'
+import { generateMenuItems, generateLinkMenuItems, generateRouteMenuItems } from '../../shared/generatorUtils/GeneratorExtension'
 import Footer from '../footer/FooterComponent'
 import { AppBar, Box, Fade, Paper, Typography } from '@material-ui/core'
 import { layoutStyle } from './main-layout-style'
@@ -13,9 +13,9 @@ import FooterSection from '../footer/footer-section/FooterSectionComponent'
 const MainLayout = (props) => {
     const classes = layoutStyle();
     const fontSize = "small";
-    const accountMenu: Array<NameWithNode> = [
-        {name: "Profil", children: <PersonIcon fontSize={fontSize}/>},
-        {name: "Wyloguj", children: <ExitToAppIcon fontSize={fontSize}/>}
+    const accountMenu: Array<RouterWithElement> = [
+        {name: "Profil", route: "/profile",children: <PersonIcon fontSize={fontSize}/>},
+        {name: "Wyloguj", route: "/logout",children: <ExitToAppIcon fontSize={fontSize}/>}
     ]
     const sideBarItems: Array<RouterWithElement> = [
         {name: "Lista samochodów", route: "/login", children: <DirectionsCarIcon fontSize={fontSize}/>}
@@ -24,7 +24,7 @@ const MainLayout = (props) => {
     return (
         <React.Fragment>
             <MenuAppBar
-                accountMenuChildren={generateMenuItems(accountMenu)}
+                accountMenuChildren={generateRouteMenuItems(accountMenu)}
                 listMenu={generateLinkMenuItems(sideBarItems)}
                 isAuthorized={props.isAuthorized}
             ></MenuAppBar>
