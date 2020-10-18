d3c19d1f42956d8312549dede849c307e098d651
Kacper <kacpermatuszek2@gmail.com>
2020-10-18 22:26:31 +0200

    Added error messages to profile section component and select list in progress

--- client/components/profile/NavigationProfileDialogComponent.tsx
+++ client/components/profile/NavigationProfileDialogComponent.tsx
@@ -2,10 +2,42 @@ import { IconWithContent } from "../../core/models/base/IconWithContent"
 import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
 import ProfileSection from "./profile-section/ProfileSectionComponent";
 import { NavigationFormDialog } from "../base/form-dialog/NavigationFormDialogComponent";
+import { useState } from "react";
+import { ValidatorManage, ValidatorType } from "../login/models/validators/Validator";
 
 const NavigationProfileDialog = (props) => {
+        /*profile section errors*/
+    const [errors, setErrors] = useState({
+        firstName: "",
+        lastName: "",
+        phoneNumber: "",
+    });
+
+    const profileSectionValidatorManager = new ValidatorManage();
+    profileSectionValidatorManager.setValidators({
+        ["firstName"]: [{
+            type: ValidatorType.NotEmpty,
+            paramValue: null,
+            message: "Imię jest wymagane.",
+            isValid: true
+        }],
+        ["lastName"]: [{
+            type: ValidatorType.NotEmpty,
+            paramValue: null,
+            message: "Nazwisko jest wymagane.",
+            isValid: true
+        }],
+        ["phoneNumber"]: [{
+            type: ValidatorType.NotEmpty,
+            paramValue: null,
+            message: "Nr. telefnu jest wymagany.",
+            isValid: true,
+        }]
+    });
+
+
     const navigation: Array<IconWithContent> = [
-        {title: "Dane kontaktowe", icon: <PermContactCalendarIcon/>, content: <ProfileSection/> }
+        {title: "Dane kontaktowe", icon: <PermContactCalendarIcon/>, content: <ProfileSection errors={errors}/> }
     ];
 
     return (
--- client/components/profile/profile-section/ProfileSectionComponent.tsx
+++ client/components/profile/profile-section/ProfileSectionComponent.tsx
@@ -1,13 +1,46 @@
-import { Container, Grid, Paper, TextField } from "@material-ui/core";
+import { Container, FormControl, Grid, InputLabel, Paper, Select, TextField } from "@material-ui/core";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Virtuoso } from "react-virtuoso";
 import { profileSectionStyle } from "./profile-section-style";
 
 const ProfileSection = (props) => {
     const classes = profileSectionStyle();
+    const [totalCountry, setTotalCountry] = useState(0);
+    const [totalStateProvince, setTotalStateProvince] = useState(0);
+
+    const countryItems = useRef([]);
+    const stateProvinceItems = useRef([]);
+
+    const countryLoading = useRef(false);
+    const stateProvinceLoading = useRef(false);
+
+    const loadMoreCountries = useCallback(() => {
+        if(countryLoading.current)
+            return;
+        countryLoading.current = true;
+        //get from api
+        countryLoading.current = false
+        setTotalCountry(countryItems.current.length);
+    }, []);
+    const loadMoreStateProvinces = useCallback(() => {
+        if(stateProvinceLoading.current)
+            return;
+        stateProvinceLoading.current = true;
+        //get from api
+        stateProvinceLoading.current = false;
+        setTotalStateProvince(stateProvinceItems.current.length);
+    }, [])
+
+    useEffect(() => {
+        loadMoreCountries();
+        loadMoreStateProvinces();
+    }, [])
     return (
         <Container>
             <form className={classes.form} noValidate method="POST">
                 <TextField
                 error={!!props.errors.firstName}
+                helperText={props.errors.firstName}
                 variant="outlined"
                 margin="normal"
                 required
@@ -18,6 +51,7 @@ const ProfileSection = (props) => {
                 autoComplete="firstName" />
                 <TextField
                 error={!!props.errors.lastName}
+                helperText={props.errors.lastName}
                 variant="outlined"
                 margin="normal"
                 required
@@ -28,6 +62,7 @@ const ProfileSection = (props) => {
                 autoComplete="lastName" />
                 <TextField
                 error={!!props.errors.phoneNumber}
+                helperText={props.errors.phoneNumber}
                 variant="outlined"
                 margin="normal"
                 fullWidth
@@ -44,6 +79,40 @@ const ProfileSection = (props) => {
                 label="Nazwa firmy"
                 name="company"
                 autoComplete="company" />
+                <FormControl variant="outlined">
+                    <InputLabel id="label-select-outlined-country">Country</InputLabel>
+                    <Select labelId="label-select-outlined-country"
+                    id="select-outlined-country"
+                    label="Country">
+                        <Virtuoso
+                        overscan={500}
+                        totalCount={totalCountry}
+                        footer={() => {
+                            return (
+                                <div style={{padding: '2rem', textAlign: 'center'}}>
+                                    Loading...
+                                </div>
+                            )
+                        }}
+                        />
+                    </Select>
+                </FormControl>
+                <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="country"
+                label="Kraj"
+                name="country"
+                autoComplete="country" />
+                <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="stateProvince"
+                label="Województwo"
+                name="stateProvince"
+                autoComplete="stateProvince" />
                 <TextField
                 variant="outlined"
                 margin="normal"
