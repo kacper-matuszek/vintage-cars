4796900cef48d87a90cd9fd639cac7d9cbe84e40
Kacper <kacpermatuszek2@gmail.com>
2020-10-26 23:23:25 +0100

    Create StateProvinceView.tsx

---
+++ client/components/profile/models/StateProvinceView.tsx
@@ -0,0 +1,7 @@
+import { Guid } from "guid-typescript";
+import ISelectable from "../../../core/models/base/ISelectable";
+
+export default class StateProvinceView implements ISelectable {
+    id: Guid;
+    name: string;
+}
