b7bbe43a7e469c11056d0684bea6b4506fa58dc9
Kacper <kacpermatuszek2@gmail.com>
2020-10-26 23:22:27 +0100

    Create LogHook.tsx

---
+++ client/hooks/fetch/pagedAPI/LogHook.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import isEmpty from "../../../core/models/utils/StringExtension";
+
+const useLog = (): [(content: string) => void, string, boolean, (event?: React.SyntheticEvent, reason?: string) => void, () => void] => {
+    const [isShow, setIsShow] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const handleClose = (event, reason) => {
+        if(reason === 'clickaway')
+            return;
+        setIsShow(false);
+    }
+    const showMessage = (content: string) => setMessage(content);
+    const onClosed = () => setMessage('');
+    useEffect(() => {
+        if(isEmpty(message)) return;
+        setIsShow(true);
+        
+    }, [message]);
+
+    return [showMessage, message, isShow, handleClose, onClosed]
+}
+
+export default useLog;
