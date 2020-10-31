d41c013a858e0a6ae6d683a14e955b886325d121
Kacper <kacpermatuszek2@gmail.com>
2020-10-31 22:38:22 +0100

    Create SendSubmitHook.tsx

---
+++ client/hooks/fetch/SendSubmitHook.tsx
@@ -0,0 +1,25 @@
+import { useContext, useState } from "react";
+import NotificationContext from "../../contexts/NotificationContext";
+import isEmpty from "../../core/models/utils/StringExtension";
+import BaseWebApiService from "../../core/services/api-service/BaseWebApiService"
+import { postCallback } from "../../core/services/api-service/Callback";
+
+const useSendSubmitWithNotification = <T extends object>(url: string, showLoading: () => void, hideLoading: () => void, successMessage?: string): [send: (model: any) => Promise<void>] => {
+    const apiService = new BaseWebApiService();
+    const notification = useContext(NotificationContext);
+
+    const submit = async (object: any) => {
+        showLoading();
+        await apiService.postWithoutResponseAuthorized(`${url}`, object, postCallback(
+            () => {
+                const message = isEmpty(successMessage) ? 'Zapisano pomyślnie' : successMessage
+                notification.showSuccessMessage(message);
+            },
+            (warning) => notification.showWarningMessage(warning.message),
+            (error) => notification.showErrorMessage(error.message)
+        )).finally(() => hideLoading());
+    }
+
+    return [submit];
+}
+export default useSendSubmitWithNotification;
