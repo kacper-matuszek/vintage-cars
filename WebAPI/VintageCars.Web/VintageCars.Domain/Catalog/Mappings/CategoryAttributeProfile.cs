512ab878ace459849e9f8b65de77d7acd5f233ef
Kacper <kacpermatuszek2@gmail.com>
2020-11-12 22:00:52 +0100

    Create category attribute profile

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Catalog/Mappings/CategoryAttributeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Nop.Core.Infrastructure.Mapper;
+using VintageCars.Data.Models;
+using VintageCars.Domain.Catalog.Commands;
+
+namespace VintageCars.Domain.Catalog.Mappings
+{
+    public class CategoryAttributeProfile : Profile, IOrderedMapperProfile
+    {
+        public CategoryAttributeProfile()
+        {
+            CreateMap<CreateCategoryAttributeCommand, CategoryAttribute>();
+        }
+        public int Order => 5;
+    }
+}
