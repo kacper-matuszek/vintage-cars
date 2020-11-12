e101ef6dc590caafec13a2c3063660c3cae0cacb
Kacper <kacpermatuszek2@gmail.com>
2020-11-12 21:54:44 +0100

    Create "Create category attribute command"

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Catalog/Commands/CreateCategoryAttributeCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using VintageCars.Domain.Commands.Base;
+
+namespace VintageCars.Domain.Catalog.Commands
+{
+    public class CreateCategoryAttributeCommand : AuthorizationCommandBase
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
