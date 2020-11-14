eec369931ffaec42a38e846ca594ba55b53340a9
Kacper <kacpermatuszek2@gmail.com>
2020-11-14 21:42:48 +0100

    Create AutomapperProfileExtension.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Extensions/AutomapperProfileExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoMapper;
+using Nop.Core;
+using VintageCars.Domain.Base;
+
+namespace VintageCars.Domain.Extensions
+{
+    public static class AutomapperProfileExtension
+    {
+        public static IMappingExpression<TSource, TDestination> GenerateId<TSource, TDestination>(this IMappingExpression<TSource, TDestination> mappingExpression)
+            where TSource : IBusinessEntity
+            where TDestination : BaseEntity
+        {
+            return mappingExpression.ForMember(dest => dest.Id,
+                opt => opt.MapFrom(src => src != null && src.Id.HasValue ? src.Id.Value : Guid.NewGuid()));
+        }
+    }
+}
