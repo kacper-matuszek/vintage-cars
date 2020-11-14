cffe9953e035fb090f91c9c1058a6734b72bb4e0
Kacper <kacpermatuszek2@gmail.com>
2020-11-14 21:43:48 +0100

    Create CreateCategoryHandler.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Service/Catalog/Handlers/CreateCategoryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Infrastructure.Mapper;
+using Nop.Service.Catalog;
+using VintageCars.Domain.Catalog.Commands;
+
+namespace VintageCars.Service.Catalog.Handlers
+{
+    public class CreateCategoryHandler : IRequestHandler<CreateCategoryCommand, Unit>
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CreateCategoryHandler(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public Task<Unit> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            if (!request.Id.HasValue)
+            {
+                var category = AutoMapperConfiguration.Mapper.Map<Category>(request);
+                _categoryService.InsertCategory(category);
+
+                return Unit.Task;
+            }
+
+            var updateCategory = _categoryService.GetCategoryById(request.Id.Value);
+            updateCategory = AutoMapperConfiguration.Mapper.Map(request, updateCategory);
+            _categoryService.UpdateCategory(updateCategory);
+
+            return Unit.Task;
+        }
+    }
+}
