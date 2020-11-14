2803aa2372407b3b4bc14392e44fb740eef430bb
Kacper <kacpermatuszek2@gmail.com>
2020-11-14 21:43:55 +0100

    Create CreateCategoryCommand.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Catalog/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using VintageCars.Domain.Base;
+using VintageCars.Domain.Commands.Base;
+
+namespace VintageCars.Domain.Catalog.Commands
+{
+    public class CreateCategoryCommand : CommandBase, IBusinessEntity
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
