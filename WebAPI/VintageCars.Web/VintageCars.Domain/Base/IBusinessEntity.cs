32826213f9a073a3e65b209c2092e16401fb27b1
Kacper <kacpermatuszek2@gmail.com>
2020-11-14 21:46:29 +0100

    Create IBusinessEntity.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Base/IBusinessEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VintageCars.Domain.Base
+{
+    public interface IBusinessEntity
+    {
+        Guid? Id { get; set; }
+    }
+}
