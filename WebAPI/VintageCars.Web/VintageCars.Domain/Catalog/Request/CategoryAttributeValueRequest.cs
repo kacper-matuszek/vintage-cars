da7d6dc4d26f53edbd80f96c2104d8c341b31b8f
Kacper <kacpermatuszek2@gmail.com>
2020-11-15 16:53:24 +0100

    Create CategoryAttributeValueRequest.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Catalog/Request/CategoryAttributeValueRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using VintageCars.Domain.Base;
+
+namespace VintageCars.Domain.Catalog.Request
+{
+    public class CategoryAttributeValueRequest : IBusinessEntity
+    {
+        public Guid? Id { get; set; }
+        public Guid CategoryAttributeId { get; set; }
+        public string Name { get; set; }
+        public bool IsPreSelected { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
