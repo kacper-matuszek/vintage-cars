7fb9e3f15c7567080a60fac762199932a307a1e3
Kacper <kacpermatuszek2@gmail.com>
2020-11-15 18:19:28 +0100

    Fix namespace in DeleteCategoryAttributeValueHandler

--- WebAPI/VintageCars.Web/VintageCars.Service/Catalog/Handlers/DeleteCategoryAttributeValueHandler.cs
+++ WebAPI/VintageCars.Web/VintageCars.Service/Catalog/Handlers/DeleteCategoryAttributeValueHandler.cs
@@ -1,9 +1,9 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using Microsoft.Rest;
 using Nop.Service.Localization;
 using VintageCars.Domain.Catalog.Commands;
+using VintageCars.Domain.Exceptions;
 using VintageCars.Service.Catalog.Services;
 
 namespace VintageCars.Service.Catalog.Handlers
@@ -22,7 +22,7 @@ namespace VintageCars.Service.Catalog.Handlers
         public Task<Unit> Handle(DeleteCategoryAttributeValueCommand request, CancellationToken cancellationToken)
         {
             var categoryAttributeValue = _extendedCategoryService.GetCategoryAttributeValueById(request.Id);
-            if(categoryAttributeValue is null)
+            if(categoryAttributeValue == null)
                 throw new ValidationException(_localizationService.GetResource("DeleteCategoryAttributeValue.NotExists.Validation"));
 
             _extendedCategoryService.DeleteCategoryAttributeValue(categoryAttributeValue);
