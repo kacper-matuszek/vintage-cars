e23e177a5ae50b7402ceab3d03bb58a2329ea070
Kacper <kacpermatuszek2@gmail.com>
2020-11-15 11:22:37 +0100

    Name category and attribute validation.

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Catalog/Validators/CreateUpdateCategoryValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using FluentValidation;
+using VintageCars.Domain.Base.Validators;
+using VintageCars.Domain.Catalog.Commands;
+
+namespace VintageCars.Domain.Catalog.Validators
+{
+    public class CreateUpdateCategoryValidator : BaseValidator<CreateUpdateCategoryCommand>
+    {
+        public CreateUpdateCategoryValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithMessage(GetMessageFromKey("CategoryAttribute.CreateCategoryAttribute.NameEmpty.Validation"));
+            RuleFor(c => c.AttributeMappings)
+                .Must(am => am == null || am.GroupBy(m => m.CategoryAttributeId).Any(m => m.Count() == 1))
+                .WithMessage(GetMessageFromKey("CategoryAttributeMapping.Attribute.Validation"));
+        }
+    }
+}
