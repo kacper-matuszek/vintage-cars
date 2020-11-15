70199b3a9c69f0e37610338680ca4bf6acb78913
Kacper <kacpermatuszek2@gmail.com>
2020-11-15 16:53:31 +0100

    Create LinkCategoryAttributeValueCommand.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Catalog/Commands/LinkCategoryAttributeValueCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using VintageCars.Domain.Catalog.Request;
+using VintageCars.Domain.Commands.Base;
+
+namespace VintageCars.Domain.Catalog.Commands
+{
+    public class LinkCategoryAttributeValueCommand : CommandBase
+    {
+        public Guid CategoryId { get; set; }
+        public IEnumerable<CategoryAttributeValueRequest> CategoryAttributeValues { get; set; }
+    }
+}
