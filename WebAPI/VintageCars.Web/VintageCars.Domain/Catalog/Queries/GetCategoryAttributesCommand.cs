62332be832f3468577e431b29674e0b78d98163a
Kacper <kacpermatuszek2@gmail.com>
2020-11-21 11:18:27 +0100

    Created command and handler for paged list of category attributes.

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Catalog/Queries/GetCategoryAttributesCommand.cs
@@ -0,0 +1,18 @@
+﻿using VintageCars.Domain.Catalog.Response;
+using VintageCars.Domain.Commands.Base;
+using VintageCars.Domain.Common;
+using VintageCars.Domain.Utils;
+
+namespace VintageCars.Domain.Catalog.Queries
+{
+    public class GetCategoryAttributesCommand : QueryPagedBase<PagedList<CategoryAttributeView>>
+    {
+        public GetCategoryAttributesCommand()
+        {
+        }
+
+        public GetCategoryAttributesCommand(PagedRequest paged) : base(paged)
+        {
+        }
+    }
+}
---
+++ WebAPI/VintageCars.Web/VintageCars.Service/Catalog/Handlers/GetCategoryAttributesHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using VintageCars.Data.Models;
+using VintageCars.Domain.Catalog.Queries;
+using VintageCars.Domain.Catalog.Response;
+using VintageCars.Domain.Common;
+using VintageCars.Domain.Extensions;
+using VintageCars.Service.Catalog.Services;
+
+namespace VintageCars.Service.Catalog.Handlers
+{
+    public class GetCategoryAttributesHandler : IRequestHandler<GetCategoryAttributesCommand, PagedList<CategoryAttributeView>>
+    {
+        private readonly IExtendedCategoryService _extendedCategoryService;
+
+        public GetCategoryAttributesHandler(IExtendedCategoryService extendedCategoryService)
+        {
+            _extendedCategoryService = extendedCategoryService;
+        }
+
+        public Task<PagedList<CategoryAttributeView>> Handle(GetCategoryAttributesCommand request, CancellationToken cancellationToken)
+        {
+            var pagedList = _extendedCategoryService.GetPagedCategoryAttributes(request.Paged.PageIndex, request.Paged.PageSize);
+            return Task.FromResult(pagedList.ConvertPagedList<CategoryAttribute, CategoryAttributeView>());
+        }
+    }
+}
