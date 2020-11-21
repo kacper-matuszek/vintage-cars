5204619f1ef462dc98f59168dbf34d9fedd2f696
Kacper <kacpermatuszek2@gmail.com>
2020-11-21 11:18:34 +0100

    Create CategoryAttributeView.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Catalog/Response/CategoryAttributeView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VintageCars.Domain.Catalog.Response
+{
+    public class CategoryAttributeView
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
