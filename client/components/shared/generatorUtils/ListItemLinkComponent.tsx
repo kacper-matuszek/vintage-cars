311d68902be8f258020ed2329c580018813cc7e3
Kacper <kacpermatuszek2@gmail.com>
2020-11-21 01:57:30 +0100

    Update ListItemLinkComponent.tsx

--- client/components/shared/generatorUtils/ListItemLinkComponent.tsx
+++ client/components/shared/generatorUtils/ListItemLinkComponent.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import Link from 'next/link'
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import { RouterWithElement } from '../../../core/models/base/NameWithNode';
+import { Collapse, List } from '@material-ui/core';
+import { generateLinkMenuItems } from './GeneratorExtension';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import isEmpty from '../../../core/models/utils/StringExtension';
 
 interface ListItemLinkProps {
     icon?: React.ReactElement;
     primary: string;
     to: string;
+    collapseElements: Array<RouterWithElement>;
 }
 
 export const ListItemLink = (props: ListItemLinkProps) => {
-    const { icon, primary, to } = props;
-  
+    const { icon, primary, to, collapseElements } = props;
+    const [open, setOpen] = useState(false);
+    const [isCurrent, setIsCurrent] = useState(false);
+    const handleClick = () => {
+      setOpen(!open);
+    };
+
+    const hasCollapseItems = () => 
+        collapseElements !== undefined && collapseElements.length > 0;
+
+    useEffect(() => {
+      if(!isEmpty(to) && window.location.href.endsWith(to)) {
+        setIsCurrent(true);
+        return;
+      }
+      setIsCurrent(false);
+    }, []);
     return (
         <li>
-          <Link href={to}>
-            <ListItem button>
+          {!hasCollapseItems() ? 
+            <Link href={to}>
+              <ListItem button selected={isCurrent}>
+                {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+                <ListItemText primary={primary} />
+              </ListItem>
+            </Link> : 
+            <>
+            <ListItem button onClick={handleClick}>
               {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
-              <ListItemText primary={primary} />
+              <ListItemText primary={primary}/>
+             {open ? <ExpandLess /> : <ExpandMore/>}
             </ListItem>
-          </Link>
+            <Collapse in={open} timeout="auto">
+              <List component="div" disablePadding>
+                {generateLinkMenuItems(collapseElements)}
+              </List>
+            </Collapse>
+            </>
+          }
         </li>
     );
 }
