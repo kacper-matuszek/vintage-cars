b07e2ca4a8f3db96c0dbfdd2c5b70350fc8b7e3c
Kacper <kacpermatuszek2@gmail.com>
2020-11-21 17:47:15 +0100

    Create TableContentComponent.tsx

---
+++ client/components/base/table-list/table-content/TableContentComponent.tsx
@@ -0,0 +1,12 @@
+interface TableContentProps {
+    headerName: string,
+    cellValue: any
+}
+
+const TableContent = (props: TableContentProps) => {
+    const {headerName, cellValue} = props;
+
+    return <></>
+}
+
+export default TableContent;
