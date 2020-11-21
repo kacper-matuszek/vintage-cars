6f70dcf286590b2c579cbe87926a0976d3dd4049
Kacper <kacpermatuszek2@gmail.com>
2020-11-21 16:48:58 +0100

    Created TableToolbar

---
+++ client/components/base/table-list/table-toolbar/TableToolbarComponent.tsx
@@ -0,0 +1,39 @@
+import { IconButton, Toolbar, Tooltip, Typography } from "@material-ui/core";
+import useStyles from "./table-toolbar-style";
+import clsx from "clsx";
+import DeleteIcon from '@material-ui/icons/Delete';
+
+interface TableToolbarProps {
+    numSelected: number
+}
+
+const TableToolbar = (props: TableToolbarProps) => {
+    const classes = useStyles();
+    const { numSelected }  = props;
+
+    return (
+        <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+          Nutrition
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : ( <></> )}
+    </Toolbar>
+    );
+};
+export default TableToolbar;
