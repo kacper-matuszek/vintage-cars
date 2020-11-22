e6ece9ddc5b9610892695420c239dec47d33747c
Kacper <kacpermatuszek2@gmail.com>
2020-11-22 21:34:40 +0100

    Create CategoryAttributeList.tsx

---
+++ client/components/admin/categories/category-attributes/category-attributes-list/CategoryAttributeList.tsx
@@ -0,0 +1,33 @@
+import ExtendedTable from "../../../../base/table-list/extended-table/ExtendedTableComponent"
+import CategoryAttributeView from "../models/CategoryAttributeView"
+import ReactElement from "react"
+import TableContent from "../../../../base/table-list/table-content/TableContentComponent";
+import { HeadCell } from "../../../../base/table-list/table-head/HeadCell";
+import { Guid } from "guid-typescript";
+
+const rows: Array<CategoryAttributeView> = [
+    {id: Guid.create(), name: "tst", description: "desc"},
+    {id: Guid.create(), name: "tst2", description: "desc2"},
+    {id: Guid.create(), name: "tst3", description: "desc3"}
+
+]
+const headers: HeadCell<CategoryAttributeView>[] = [
+    {id: 'name', label: 'nazwa'},
+    {id: 'description', label: 'Opis'},
+    
+]
+const CategoryAttributeList = () => {
+    return (
+        <ExtendedTable<CategoryAttributeView>
+            rows={rows}
+            title="Atrybuty Kategorii"
+        >
+            {headers.map(obj => {
+                return (
+                    <TableContent name={obj.id} headerName="nazwa"/>
+                )
+            })}
+        </ExtendedTable>
+    )
+}
+export default CategoryAttributeList;
