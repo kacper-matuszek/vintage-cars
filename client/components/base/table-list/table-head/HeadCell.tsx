0f7ac1308498cd0c81229f5c79d06c3c587249d7
Kacper <kacpermatuszek2@gmail.com>
2020-11-25 22:23:12 +0100

    Created simple head cell

--- client/components/base/table-list/table-head/HeadCell.tsx
+++ client/components/base/table-list/table-head/HeadCell.tsx
@@ -2,3 +2,8 @@ export interface HeadCell<T> {
     id: keyof T,
     label: string
 }
+export interface SimpleHeadCell {
+    label: string,
+    visible: boolean,
+    width?: string, //width in px
+}
