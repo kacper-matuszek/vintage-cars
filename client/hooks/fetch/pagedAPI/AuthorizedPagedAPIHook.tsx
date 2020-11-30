560e11042696f8807cf13cd53d0f5b3ee1995df4
Kacper <kacpermatuszek2@gmail.com>
2020-11-30 22:31:38 +0100

    Refresh

--- client/hooks/fetch/pagedAPI/AuthorizedPagedAPIHook.tsx
+++ client/hooks/fetch/pagedAPI/AuthorizedPagedAPIHook.tsx
@@ -6,7 +6,7 @@ import PagedList from "../../../core/models/paged/PagedList";
 import BaseWebApiService from "../../../core/services/api-service/BaseWebApiService";
 import { toCallback } from "../../../core/services/api-service/Callback";
 
-const useAuhtorizationPagedList = <T extends IModel>(url: string, onError?: (message: string) => void): [Dispatch<SetStateAction<Paged>>, boolean, PagedList<T>] => {
+const useAuhtorizationPagedList = <T extends IModel>(url: string, onError?: (message: string) => void): [Dispatch<SetStateAction<Paged>>, boolean, PagedList<T>, () => Promise<void>] => {
     const [isLoading, setIsLoading] = useState(false);
     const [response, setResponse] = useState(new PagedList<T>());
     const [paged, setPaged] = useState<Paged>(null);
@@ -57,11 +57,13 @@ const useAuhtorizationPagedList = <T extends IModel>(url: string, onError?: (mes
         }
     }
     
+    const refresh = () => getData();
+
     useEffect(() => {
         if(paged === null) return;
         getData();
     }, [paged]);
 
-    return [setPaged, isLoading, response];
+    return [setPaged, isLoading, response, refresh];
 }
 export default useAuhtorizationPagedList;
