e1d5cd3268b200fd1479f5b827bd8b4288a0ad6a
Kacper <kacpermatuszek2@gmail.com>
2020-12-02 21:25:41 +0100

    Create CategoryAttributeMappingView.tsx

---
+++ client/components/admin/categories/categories-list/models/CategoryAttributeMappingView.tsx
@@ -0,0 +1,6 @@
+import { AttributeControlType } from "../../../../../core/models/enums/AttributeControlType";
+import CategoryAttributeView from "../../category-attributes/models/CategoryAttributeView";
+
+export default class CategoryAttributeMappingView extends CategoryAttributeView {
+    public attributeControlType: AttributeControlType;
+}
