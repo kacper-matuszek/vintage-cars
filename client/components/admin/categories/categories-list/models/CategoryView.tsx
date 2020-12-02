7dd024d23b9c46e8a5c73d4420faaa07bfa11891
Kacper <kacpermatuszek2@gmail.com>
2020-12-02 21:25:45 +0100

    Create CategoryView.tsx

---
+++ client/components/admin/categories/categories-list/models/CategoryView.tsx
@@ -0,0 +1,13 @@
+import { Guid } from "guid-typescript";
+import ISelectableArchival from "../../../../../core/models/base/ISelectableArchival";
+import CategoryAttributeMappingView from "./CategoryAttributeMappingView";
+
+export default class CategoryView implements ISelectableArchival {
+    id: Guid;
+    name: string;
+    description: string;
+    isPublished: boolean;
+    showOnHomePag: boolean;
+    isArchival: boolean;
+    attributes: Array<CategoryAttributeMappingView>;
+}
