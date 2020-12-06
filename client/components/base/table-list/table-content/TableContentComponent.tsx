16e6496e33540c06b63153fcc74010288292001d
Kacper <kacpermatuszek2@gmail.com>
2020-12-06 14:00:16 +0100

    Update TableContentComponent.tsx

--- client/components/base/table-list/table-content/TableContentComponent.tsx
+++ client/components/base/table-list/table-content/TableContentComponent.tsx
@@ -1,10 +1,11 @@
 interface TableContentProps {
     name: string;
     headerName: string;
+    content?: (model: any) => JSX.Element;
 }
 
 const TableContent = (props: TableContentProps) => {
-    const {name, headerName} = props;
+    const {name, headerName, content} = props;
 
     return <></>
 }
