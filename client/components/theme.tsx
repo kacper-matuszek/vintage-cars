f935ce8dd10f83d34f2530e5d0341413b7f6adae
Kacper <kacpermatuszek2@gmail.com>
2020-12-10 00:08:35 +0100

    Fixed theme

--- client/components/theme.tsx
+++ client/components/theme.tsx
@@ -12,29 +12,37 @@ export const theme = createMuiTheme({
             contrastText: '#FFF',
         }
     },
-    overrides: {
+    components: {
         MuiCssBaseline: {
-            '@global': {
-                '*::-webkit-scrollbar': {
-                    width: '0.7em',
-                  },
-                  '*::-webkit-scrollbar-track': {
-                    '-webkit-box-shadow': 'inset 0 0 6px rgba(255,255,255,0.9)'
-                  },
-                  '*::-webkit-scrollbar-thumb': {
-                    backgroundColor: 'rgba(146,123,89,1)',
-                  }
+            styleOverrides: {
+                '@global': {
+                    '*::-webkit-scrollbar': {
+                        width: '0.7em',
+                      },
+                      '*::-webkit-scrollbar-track': {
+                        '-webkit-box-shadow': 'inset 0 0 6px rgba(255,255,255,0.9)'
+                      },
+                      '*::-webkit-scrollbar-thumb': {
+                        backgroundColor: 'rgba(146,123,89,1)',
+                      }
+                }
             }
         },
         MuiListItem: {
-            root: {
-                "&$selected": {
-                  backgroundColor: "#c3aa86"
+            styleOverrides: {
+                root: {
+                    "&$selected": {
+                      backgroundColor: "#c3aa86"
+                    }
                 }
             }
         },
         MuiTab: {
-            fullWidth: true
+            styleOverrides: {
+                root: {
+                    fullWidth: true
+                }
+            }
         }
     }
 });
