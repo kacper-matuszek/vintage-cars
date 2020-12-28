383f6e54152892c829deb4778fe4d07f66029b68
Kacper <kacpermatuszek2@gmail.com>
2020-12-28 12:01:09 +0100

    Create BaseCreationEntity.cs

---
+++ WebAPI/VintageCars.Web/VintageCars.Data/Models/Base/BaseCreationEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nop.Core;
+
+namespace VintageCars.Data.Models.Base
+{
+    public abstract class BaseCreationEntity : BaseEntity
+    {
+        protected BaseCreationEntity()
+        : base()
+        {
+            CreateDate = DateTime.UtcNow;
+        }
+        public int CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
