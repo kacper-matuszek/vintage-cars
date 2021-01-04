75c52de55ba07c18597ee04d40e213396257efcd
Kacper <kacpermatuszek2@gmail.com>
2021-01-04 19:30:06 +0100

    Fix in product announcement.

--- WebAPI/VintageCars.Web/VintageCars.Data/Models/ProductAnnouncementAttribute.cs
+++ WebAPI/VintageCars.Web/VintageCars.Data/Models/ProductAnnouncementAttribute.cs
@@ -6,7 +6,7 @@ namespace VintageCars.Data.Models
     public class ProductAnnouncementAttribute : BaseEntity
     {
         public Guid CategoryAttributeId { get; set; }
-        public Guid CategoryAttributeValueId { get; set; }
+        public Guid? CategoryAttributeValueId { get; set; }
         public string Value { get; set; }
     }
 }
--- WebAPI/VintageCars.Web/VintageCars.Service/ProductAnnouncement/Handlers/CreateProductAnnouncementHandler.cs
+++ WebAPI/VintageCars.Web/VintageCars.Service/ProductAnnouncement/Handlers/CreateProductAnnouncementHandler.cs
@@ -5,7 +5,6 @@ using MediatR;
 using Nop.Core.Infrastructure.Mapper;
 using VintageCars.Data.Models;
 using VintageCars.Domain.ProductAnnouncement.Commands;
-using VintageCars.Service.Infrastructure;
 using VintageCars.Service.ProductAnnouncement.Services;
 
 namespace VintageCars.Service.ProductAnnouncement.Handlers
@@ -13,12 +12,10 @@ namespace VintageCars.Service.ProductAnnouncement.Handlers
     public class CreateProductAnnouncementHandler : IRequestHandler<CreateProductAnnouncement, Unit>
     {
         private readonly IProductAnnouncementService _productAnnouncementService;
-        private readonly IInfrastructureService _infrastructureService;
 
-        public CreateProductAnnouncementHandler(IProductAnnouncementService productAnnouncementService, IInfrastructureService infrastructureService)
+        public CreateProductAnnouncementHandler(IProductAnnouncementService productAnnouncementService)
         {
             _productAnnouncementService = productAnnouncementService;
-            _infrastructureService = infrastructureService;
         }
 
         public Task<Unit> Handle(CreateProductAnnouncement request, CancellationToken cancellationToken)
@@ -27,7 +24,7 @@ namespace VintageCars.Service.ProductAnnouncement.Handlers
             _productAnnouncementService.InsertProductAnnouncement(productAnnouncement);
 
             var productAnnouncementAttributes = AutoMapperConfiguration.Mapper.Map<List<ProductAnnouncementAttribute>>(request.Attributes);
-            productAnnouncementAttributes.ForEach(paa =>
+            productAnnouncementAttributes?.ForEach(paa =>
             {
                 _productAnnouncementService.InsertProductAnnouncementAttribute(paa);
                 _productAnnouncementService.InsertProductAnnouncementAttributeMappings(new ProductAnnouncementAttributeMapping()
@@ -37,7 +34,7 @@ namespace VintageCars.Service.ProductAnnouncement.Handlers
                 });
             });
 
-            request.Pictures.ForEach(picture =>
+            request.Pictures?.ForEach(picture =>
             {
                 _productAnnouncementService.InsertProductAnnouncementPictureMapping(picture, productAnnouncement.Id);
             });
