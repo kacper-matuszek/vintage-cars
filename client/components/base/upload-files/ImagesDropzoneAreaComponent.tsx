31f40b31b8cfab4d7a163bec23d4a8597280cb38
Kacper <kacpermatuszek2@gmail.com>
2021-01-14 23:31:16 +0100

    Fixed uploading multiple files and deleting file.

--- client/components/base/upload-files/ImagesDropzoneAreaComponent.tsx
+++ client/components/base/upload-files/ImagesDropzoneAreaComponent.tsx
@@ -7,21 +7,38 @@ interface ImagesDropzoneAreaProps {
 }
 
 const ImagesDropzoneArea = (props: ImagesDropzoneAreaProps) => {
+    const toBase64 = (data: string) => data.split(',')[1];
     const onDropHandler = (files) => {
-        let file = files[0];
+        let promises = [];
+        for(let file of files) {
+            let filePromise = new Promise(resolve => {
+                let reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = () => resolve(new File(file.name, file.path, file.type, toBase64(reader.result as string), file.size, new Date(file.lastModified)));
+            });
+            promises.push(filePromise);
+        }
+        Promise.all(promises).then(fileContents => {
+            props.setFiles(prevState => {
+                prevState.push(...fileContents);
+                return prevState;
+            })
+        })
+    }
+    const onDeleteHandler = (file) => {
         const reader = new FileReader();
         reader.onload = (event) => {
             const data = event.target.result as string;
             props.setFiles(prevState => {
-                prevState.push(new File(file.name, file.path, file.type, data, file.size, new Date(file.lastModified)))
-                return prevState;
-            })
+                return prevState.filter(x => x.dataAsBase64 !== toBase64(data));
+            });
         };
-      reader.readAsDataURL(file);
+        reader.readAsDataURL(file);
     }
     return (
         <DropzoneArea 
             onDrop={onDropHandler}
+            onDelete={onDeleteHandler}
             showFileNames
             showAlerts={false}
             dropzoneText={"Przeciągnij i upuść obrazek lub kliknij"}
