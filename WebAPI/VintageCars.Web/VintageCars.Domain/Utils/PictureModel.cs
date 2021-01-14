2f54917947b812f757e56ff0ecdc06ed9f439df3
Kacper <kacpermatuszek2@gmail.com>
2021-01-15 00:49:44 +0100

    Updated PictureModel

--- WebAPI/VintageCars.Web/VintageCars.Domain/Utils/PictureModel.cs
+++ WebAPI/VintageCars.Web/VintageCars.Domain/Utils/PictureModel.cs
@@ -1,4 +1,5 @@
-﻿using Nop.Core.Domain.Media;
+﻿using System;
+using Nop.Core.Domain.Media;
 
 namespace VintageCars.Domain.Utils
 {
@@ -7,5 +8,15 @@ namespace VintageCars.Domain.Utils
         public Picture Picture { get; set; }
         public string DataAsBase64 { get; set; }
         public byte[] DataAsByteArray { get; set; }
+
+        public byte[] GetDataAsByteArray()
+        {
+            return DataAsByteArray.Length > 0 ? DataAsByteArray : Convert.FromBase64String(DataAsBase64);
+        }
+
+        public string GetDataAsBase64()
+        {
+            return DataAsByteArray.Length > 0 ? Convert.ToBase64String(DataAsByteArray) : DataAsBase64;
+        }
     }
 }
